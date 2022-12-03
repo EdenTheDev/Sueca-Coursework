dd9c3d00c48d912a712d2ae4888bb8b877ddb64f
EdenTheDev <118276354+EdenTheDev@users.noreply.github.com>
2022-12-03 00:11:47 +0000

    Update sueca_cards.py

--- Python Code/sueca_cards.py
+++ Python Code/sueca_cards.py
@@ -16,20 +16,44 @@ class card:
         if self.rank == "Q":
             return "2"
         if self.rank == "8" or "9" or "10":
-            return "Invalid card symbol"
+            return "0"
+        else:
+            raise CardInvalid(f"Invalid suit symbol {self}")
+
             
     def higher_than(self, other, s, t):
-        return""
+        if self.suit == other.suit:
+            if self.rank > other.rank:
+                return True
+            else:
+                return False
+
+        elif self.suit != other.suit:
+            return "different suits"
+            
     
     def show(self):
-        return""
+        card =(self.rank+self.suit)
+        return card
     
 class CardInvalid(Exception):
     pass
        
-def parseCard(cs):
-    if True:
-        print("")
+def parseCard(cs: str):
+    cardList = ["2H","3H","4H","5H","6H","7H","QH","JH","KH","AH",
+                "2C","3C","4C","5C","6C","7C","QC","JC","KC","AC",
+                "2S","3S","4S","5S","6S","7S","QS","JS","KS","AS",
+                "2D","3D","4D","5D","6D","7D","QD","JD","KD", "AD"
+                ]
+
+    if cs in cardList:
+        print("test")
+        return card(cs[0],cs[1])
+    elif len(cs) != 2:
+        raise CardInvalid(f"Invalid card length {cs}")
     else:
         raise CardInvalid(f"Invalid card symbol: {cs}")
     
+
+
+print(parseCard("KS"))                  
