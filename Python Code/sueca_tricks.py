efc9fd5e1caf905d705a8caddf16bd710a1bc0be
EdenTheDev <118276354+EdenTheDev@users.noreply.github.com>
2022-12-03 00:18:10 +0000

    Create sueca_tricks.py

---
+++ Python Code/sueca_tricks.py
@@ -0,0 +1,17 @@
+class Tricks:
+    def __init__(self):
+        return
+    def points(self):
+        return
+    def trick_winner(t):
+        return
+    def show(self):
+        return
+    
+    pass
+
+def parseTrick(ts):
+    return
+
+def parseGameFile(fname):
+    return
